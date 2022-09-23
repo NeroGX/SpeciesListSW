80565d6696fd2d69941a49825fce5585ed1ff17c
Joseph Robinson <josephr1981@live.com>
2022-09-22 21:06:41 -0400

    Imported the HttpClientModule and added the SpeciesComponent module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SpeciesComponent } from './species/species.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpeciesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
